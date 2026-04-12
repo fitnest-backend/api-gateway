f37774db0a2083324be35fe11ea246a14743b3e5
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-04-12 23:29:37 +0400

    fix: add missing return statement in securityFilterChain method

--- src/main/java/az/fitnest/gateway/ApiGatewayApplication.java
+++ src/main/java/az/fitnest/gateway/ApiGatewayApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
     "org.springframework.cloud.autoconfigure.LifecycleMvcEndpointAutoConfiguration",
     "org.springframework.cloud.autoconfigure.RefreshAutoConfiguration"
 })
-@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
