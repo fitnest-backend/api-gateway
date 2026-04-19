9775604cf273496b1d28f0cd753331e9847af042
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-04-19 14:26:27 +0400

    chore: update addTrainerAvailability method to include lessonId parameter and adjust API endpoint

--- src/main/java/az/fitnest/gateway/ApiGatewayApplication.java
+++ src/main/java/az/fitnest/gateway/ApiGatewayApplication.java
@@ -92,7 +92,7 @@ public class ApiGatewayApplication {
                         "/api/v1/admin/gyms/all",
                         "/api/v1/admin/reservations/gyms/{id}/enable",
                         "/api/v1/admin/reservations/gyms/{id}/trainers/{trainerId}/enable/{lessonId}",
-                        "/api/v1/admin/reservations/gyms/{id}/trainers/{trainerId}/availabilities",
+                        "/api/v1/admin/reservations/gyms/{id}/trainers/{trainerId}/lessons/{lessonId}/availabilities",
                         "/api/v1/admin/reservations/{reservationId}/status",
                         "/api/v1/admin/reservations/gyms/{id}/lesson-types",
                         "/api/v1/admin/reservations/gyms/{id}/lesson-types/{lessonTypeId}",
